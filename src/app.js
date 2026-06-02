a4f8096d173bfd02eaf1317dd2645a804e298f3a
pateldrup <pateldrup.cg@gmail.com>
2026-06-02 22:35:40 +0530

    chore: add vercel configuration and serverless database integration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const noteRoutes = require("./routes/note.routes");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use(express.json());
 
